df4e423e4078921e6f516acac0ea8657439328b4
kevintene <kevin.tene99@gmail.com>
2025-05-04 22:08:22 +0200

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,11 @@
+This is a personal-use-only project.
+
+I do not own the rights to the backgrounds and icons used. Credits go to the original creators:
+
+Backgrounds:
+Sourced from an Internet Archive post by xodus02
+🔗 https://archive.org/details/windows-xp-hd-wallpaper-pack-5.0.7z
+
+Icons:
+Sourced from a DeviantArt post by marchmountain
+🔗 https://www.deviantart.com/marchmountain/art/Windows-XP-High-Resolution-Icon-Pack-916042853
---
+++ script.js
@@ -0,0 +1,59 @@
+// Funzione per ottenere l'ora corrente in formato 12 ore (AM/PM)
+function updateTime() {
+    const now = new Date();
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let ampm = hours >= 12 ? 'PM' : 'AM';
+    
+    // Convertire l'ora in formato 12 ore
+    hours = hours % 12;
+    hours = hours ? hours : 12; // Se è 0, metti 12
+    minutes = minutes < 10 ? '0' + minutes : minutes; // Aggiungi lo zero a sinistra se i minuti sono minori di 10
+
+    // Crea la stringa dell'orario
+    const timeString = hours + '.' + minutes + ' ' + ampm;
+
+    // Aggiorna il div con id 'clock' con il nuovo orario
+    document.getElementById('clock').textContent = timeString;
+}
+
+// Chiamata iniziale per visualizzare subito l'ora
+updateTime();
+
+// Aggiorna ogni 1000 ms (1 secondo)
+setInterval(updateTime, 1000);
+
+
+
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+  };
+
+
+
+
+  const backgrounds = [
+    'url("backgrounds/Moon flower (Unedited Version).jpg")',
+    'url("backgrounds/Red moon desert.jpg")',
+    'url("backgrounds/Wind.jpg")',
+    'url("backgrounds/Vortec space.jpg")',
+    'url("backgrounds/Tulips.jpg")',
+    'url("backgrounds/Stonehenge (Getty Images Rescan).jpg")',
+    'url("backgrounds/Follow.jpg")',
+    'url("backgrounds/Azul.jpg")',
+    'url("backgrounds/Autumn.jpg")',
+    'url("backgrounds/Ascent.jpg")',
+    'url("backgrounds/Azul.jpg")',
+    'url("backgrounds/xp.webp")',
+    'url("backgrounds/xp.webp")',
+    'url("backgrounds/xp.webp")',
+    'url("backgrounds/xp.webp")',
+    'url("backgrounds/xp.webp")',
+    'url("backgrounds/xp.webp")',
+    'url("backgrounds/xp.webp")',
+    'url("backgrounds/xp.webp")',
+  ];
+
+const randomIndex = Math.floor(Math.random() * backgrounds.length);
+
+document.body.style.backgroundImage = backgrounds[randomIndex];
